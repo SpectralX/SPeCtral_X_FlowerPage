b7d3d7a5e2f41181612ca32088f8ade2baf1b3ca
SpectralX_HUN <galaxies20122@gmail.com>
2021-02-14 08:10:39 +0100

    html-->php format

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Oldal címe</title>
+</head>
+
+<body>
+    <?php  include "php/menu.php" ?>
+</body>
+
+</html>
--- php/menu.php
+++ php/menu.php
@@ -8,14 +8,14 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../css/menu.css">
+    <link rel="stylesheet" href="css/menu.css">
     <title>Oldal neve</title>
 </head>
 
 <body>
 
     <nav class="navbar">
-        <a href="/"><img class="logo_icon" src="/img/moon.png" alt="Anikó Virágai" title="Anikó Virágai"></a>
+        <a href="/"><img class="logo_icon" src="img/moon.png" alt="Anikó Virágai" title="Anikó Virágai"></a>
         <h2 class="logo_text">Oldal neve</h2>
 
         <ul class="navbar-list" id="navi-list">
@@ -41,7 +41,7 @@
             <div class="toogle_list"></div>
         </div>
     </nav>
-    <script src="../js/menu.js"></script>
+    <script src="js/menu.js"></script>
     <a href="#" class="fa fa-facebook"></a>
 </body>
 
