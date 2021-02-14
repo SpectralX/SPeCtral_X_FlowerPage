23d02b7a61e6f9849c483d3bf63ede9adebc19bb
SpectralX_HUN <galaxies20122@gmail.com>
2021-02-14 07:20:54 +0100

    menu add -->php folder

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oldal címe</title>
+</head>
+
+<body>
+    <?php  include "../php/menu.php" ?>
+</body>
+
+</html>
